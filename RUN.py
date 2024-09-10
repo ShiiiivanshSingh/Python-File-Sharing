211ac8c95593519bdbfb5bb029f9f8e39111aae9
Shivansh Pratap Singh <shivanshpratapsingh0807@gmail.com>
2024-09-10 22:56:50 +0530

    FILES PUSHED B19 (final changes + cleanup)

---
+++ RUN.py
@@ -0,0 +1,36 @@
+import http.server
+import socket
+import socketserver
+import webbrowser
+import pyqrcode
+from pyqrcode import QRCode
+import png
+import os
+
+#port decleration!
+PORT = 8010 
+os.environ['USERPROFILE']
+
+
+user_profile = os.environ.get('USERPROFILE')
+downloads_folder = os.path.join(user_profile, 'Downloads')
+os.chdir(downloads_folder)
+
+
+Handler = http.server.SimpleHTTPRequestHandler
+hostname = socket.gethostname()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+IP = "http://" + s.getsockname()[0] + ":" + str(PORT)
+link = IP
+
+url = pyqrcode.create(link)
+url.svg("myqr.svg", scale=8)
+webbrowser.open('myqr.svg')
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    print("Type this in your Browser", IP)
+    print("or Use the QRCode")
+    httpd.serve_forever()
